90b2e76ee5a8659decdb382dbd685d5317636c4c
MahmoudSalah2020 <snipermayv20@gmail.com>
2022-05-07 12:24:00 -0700

    Commit.1.2.2
    Commit.1.2.2

--- app/src/main/java/com/example/rakna/MainActivity.java
+++ app/src/main/java/com/example/rakna/MainActivity.java
@@ -5,24 +5,18 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Context;
 import android.content.Intent;
 import android.net.ConnectivityManager;
-import android.net.NetworkInfo;
 import android.os.Bundle;
-import android.os.Handler;
 import android.view.View;
-import android.widget.Button;
 import android.widget.RelativeLayout;
-import android.widget.TextView;
-
-import com.github.ybq.android.spinkit.SpinKitView;
-
-import java.net.InetAddress;
+import com.google.android.material.snackbar.BaseTransientBottomBar;
+import com.google.android.material.snackbar.Snackbar;
 
 public class MainActivity extends AppCompatActivity {
 
-    TextView noInternetTV;
-    Button retryBtn;
-    SpinKitView spinKitView;
     RelativeLayout relativeLayout;
+
+    private long milliseconds, splashTime;
+    private boolean splashActive, paused;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,66 +25,72 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         initComponents();
-        setRetryBtnListener();
 
-        if (isInternetAvailable()) {
-            createSplashHandler();
-        } else {
-            relativeLayout.setVisibility(View.VISIBLE);
-            noInternetTV.setVisibility(View.VISIBLE);
-        }
-
-    }
-
-    private void setRetryBtnListener() {
-        retryBtn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                retryBtn.setVisibility(View.INVISIBLE);
-                spinKitView.setVisibility(View.VISIBLE);
-                if(isInternetAvailable()){
-                    noInternetTV.setVisibility(View.GONE);
-                    createSplashHandler();
-                }else{
-                    delayRetryButton();
+        Thread thread = new Thread() {
+            public void run(){
+                try {
+                    while(splashActive && milliseconds < splashTime){
+                        if(!paused){
+                            milliseconds += 100;
+                            sleep(100);
+                        }
+                    }
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }finally {
+                    if(!isOnline()){
+                        Snackbar snackbar = Snackbar.make(relativeLayout, "", Snackbar.LENGTH_INDEFINITE);
+                        snackbar.addCallback(new BaseTransientBottomBar.BaseCallback<Snackbar>() {
+                            @Override
+                            public void onShown(Snackbar transientBottomBar) {
+                                super.onShown(transientBottomBar);
+                                transientBottomBar.getView().findViewById(R.id.snackbar_action).setOnClickListener(new View.OnClickListener() {
+                                    @Override
+                                    public void onClick(View view) {
+                                        if(isOnline()){
+                                            goToHome();
+                                            snackbar.dismiss();
+                                        }
+                                    }
+                                });
+                            }
+                        });
+                        snackbar.setText(getResources().getString(R.string.noInternetConnection))
+                                .setAction(getResources().getString(R.string.retry), new View.OnClickListener() {
+                                    @Override
+                                    public void onClick(View view) {
+                                    }
+                                })
+                                .setTextColor(getResources().getColor(R.color.white))
+                                .setActionTextColor(getResources().getColor(R.color.white))
+                                .setBackgroundTint(getResources().getColor(R.color.red));
+                        snackbar.show();
+                    }
+                    else {
+                        goToHome();
+                    }
                 }
             }
-        });
+        };
+
+        thread.start();
+    }
+
+    private void goToHome(){
+        startActivity(new Intent(MainActivity.this, LoginActivity.class));
+        finish();
+    }
+
+    private boolean isOnline(){
+        ConnectivityManager connectivityManager = (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
+        return connectivityManager.getActiveNetworkInfo() != null && connectivityManager.getActiveNetworkInfo().isConnectedOrConnecting();
     }
 
     private void initComponents() {
-        noInternetTV = findViewById(R.id.textView_internet_error);
-        retryBtn = findViewById(R.id.button_retry);
-        spinKitView = findViewById(R.id.progress_splash_button);
-        relativeLayout = findViewById(R.id.layout_button);
-    }
-
-    private void delayRetryButton(){
-        new Handler().postDelayed(new Runnable() {
-            @Override
-            public void run() {
-                retryBtn.setVisibility(View.VISIBLE);
-                spinKitView.setVisibility(View.INVISIBLE);
-            }
-        }, 2000);
-    }
-
-    private void createSplashHandler() {
-        new Handler().postDelayed(new Runnable() {
-            @Override
-            public void run() {
-                startActivity(new Intent(MainActivity.this, LoginActivity.class));
-                finish();
-            }
-        }, 3000);
-    }
-
-    private boolean isInternetAvailable() {
-        ConnectivityManager connectivityManager = (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
-        NetworkInfo networkInfo = connectivityManager.getActiveNetworkInfo();
-        if(networkInfo!=null){
-            return networkInfo.isConnected();
-        }else
-            return false;
+        milliseconds = 0;
+        splashTime = 900;
+        splashActive = true;
+        paused = false;
+        relativeLayout = findViewById(R.id.main_relative_layout);
     }
 }
