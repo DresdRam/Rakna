76bc9e86f83e0c09282625341afceea61a489ada
MahmoudSalah2020 <snipermayv20@gmail.com>
2022-05-07 08:00:24 -0700

    Git Desktop Commit
    Git Desktop Commit

--- app/src/main/java/com/example/rakna/Fragments/ProfileFragment.java
+++ app/src/main/java/com/example/rakna/Fragments/ProfileFragment.java
@@ -8,6 +8,8 @@ import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
 import android.net.Uri;
 import android.os.Bundle;
 
@@ -27,6 +29,7 @@ import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.example.rakna.HomeActivity;
 import com.example.rakna.R;
@@ -50,6 +53,7 @@ import com.theartofdev.edmodo.cropper.CropImageView;
 
 import java.io.IOException;
 import java.io.InputStream;
+import java.util.regex.Pattern;
 
 import de.hdodenhof.circleimageview.CircleImageView;
 import okhttp3.internal.Util;
@@ -57,8 +61,8 @@ import okhttp3.internal.Util;
 
 public class ProfileFragment extends Fragment {
     CircleImageView profileImage;
-    TextInputEditText name, email, password, phone;
-    TextView username;
+    TextInputEditText name, password, phone;
+    TextView username,userEmail;
     Button update;
     View view;
     Uri imageUri;
@@ -68,80 +72,79 @@ public class ProfileFragment extends Fragment {
     private MainViewModel mainViewModel;
 
     @Override
+    public void onAttach(@NonNull Context context) {
+        super.onAttach(context);
+        if (!isConnected()){
+            Toast.makeText(context, "Oops, Check Your Connection Please!", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         view = inflater.inflate(R.layout.fragment_profile, container, false);
         initComponent();
-        name.addTextChangedListener(textWatcher);
-        email.addTextChangedListener(textWatcher);
-        password.addTextChangedListener(textWatcher);
-        phone.addTextChangedListener(textWatcher);
+        update.setEnabled(false);
+        retrieveData();
         profileImageAction();
+        updateButtonAction();
         return view;
     }
-
-    private TextWatcher textWatcher = new TextWatcher() {
-        @Override
-        public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
-
-        }
-
-        @Override
-        public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
-            if (name.getText().length() > i2 || password.getText().length() > i2
-                    || phone.getText().length() > i2 || password.getText().length() > i2) {
-                update.setEnabled(true);
-            }
-        }
-
-        @Override
-        public void afterTextChanged(Editable editable) {
-        }
-    };
-
-    @Override
-    public void onAttach(@NonNull Context context) {
-        super.onAttach(context);
-        retrieveData();
-    }
-
-    private void retrieveData() {
-        mainViewModel = ViewModelProviders.of(this).get(MainViewModel.class);
-        mainViewModel.init();
-        mainViewModel.getProfile().observe(this, new Observer<UserModel>() {
-            @Override
-            public void onChanged(UserModel userModel) {
-                username.setText(userModel.getUserName());
-                name.setText(userModel.getUserName());
-                email.setText(userModel.getUserEmail());
-                password.setText(userModel.getUserPassword());
-                phone.setText(userModel.getUserPhone());
-                if (userModel.getUri() != null) {
-                    Picasso.get().load(userModel.getUri()).into(profileImage);
-                } else {
-                    profileImage.setImageDrawable(ContextCompat.getDrawable(getActivity(), R.drawable.profile_image));
-                }
-            }
-
-        });
-    }
-
     private void initComponent() {
         profileImage = view.findViewById(R.id.profile_image);
         name = view.findViewById(R.id.edit_name);
-        email = view.findViewById(R.id.edit_email);
         password = view.findViewById(R.id.edit_password);
         phone = view.findViewById(R.id.edit_phone);
         username = view.findViewById(R.id.retrieved_name);
         update = view.findViewById(R.id.update_button);
+        userEmail=view.findViewById(R.id.retrieved_email);
     }
+    private void retrieveData() {
+        mainViewModel = ViewModelProviders.of(this).get(MainViewModel.class);
+        mainViewModel.init();
+        mainViewModel.getProfile().observe(getViewLifecycleOwner(), new Observer<UserModel>() {
+            @Override
+            public void onChanged(UserModel userModel) {
+                username.setText(userModel.getUserName());
+                name.setText(userModel.getUserName());
+                userEmail.setText(userModel.getUserEmail());
+                password.setText(userModel.getUserPassword());
+                phone.setText(userModel.getUserPhone());
+                TextWatcher textWatcher = new TextWatcher() {
+                    @Override
+                    public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+                        update.setEnabled(true);
+                    }
+                    @Override
+                    public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
 
+                    }
+                    @Override
+                    public void afterTextChanged(Editable editable) {
+                        synchronized (this) {
+                            if (name.getText().toString() == userModel.getUserName() || password.getText().toString() == userModel.getUserPassword() || phone.getText().toString() == userModel.getUserPhone()) {
+                                update.setEnabled(false);
+                            }
+                        }
+                    }
+                };
+                name.addTextChangedListener(textWatcher);
+                password.addTextChangedListener(textWatcher);
+                phone.addTextChangedListener(textWatcher);
+                if (userModel.getUri() != null) {
+                    Picasso.get().load(userModel.getUri()).into(profileImage);
+                } else {
+                    profileImage.setImageDrawable(getResources().getDrawable(R.drawable.profile_image));
+                }
+            }
+        });
+    }
     private void profileImageAction() {
         profileImage.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 boolean pick = true;
-                if (pick == true) {
+                if (pick) {
                     if (!checkCameraPermission()) {
                         requestCameraPermission();
                     } else pickImage();
@@ -153,31 +156,78 @@ public class ProfileFragment extends Fragment {
             }
         });
     }
+    private void updateButtonAction(){
+        update.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (!nameIsValid(name.getText().toString())){
+                    name.setError("Enter Valid Name");
+                    return;
+                }if (!phoneIsValid(phone.getText().toString())){
+                    phone.setError("Enter Valid Number");
+                    return;
+                }if (name.getText().toString().isEmpty()){
+                    name.setError("Enter Your Name");
+                    return;
+                }if (phone.getText().toString().isEmpty()){
+                    phone.setError("Enter Your Phone");
+                    return;
+                }if (password.getText().toString().isEmpty()){
+                    password.setError("Enter Your Password");
+                    return;
+                }
+                reference = FirebaseDatabase.getInstance().
+                        getReference("Users").child(auth.getCurrentUser().getUid());
+                reference.child("userName").setValue(name.getText().toString());
+                reference.child("userPassword").setValue(password.getText().toString());
+                reference.child("userPhone").setValue(phone.getText().toString()).addOnSuccessListener(new OnSuccessListener<Void>() {
+                    @Override
+                    public void onSuccess(Void unused) {
+                        Toast.makeText(getActivity(), "Your Data Is Updated", Toast.LENGTH_SHORT).show();
+                        update.setEnabled(false);
+                    }
+                });
+
+
+            }
+        });
+
+
+    }
+    //regex for name
+    private boolean nameIsValid(String s) {
+        return Pattern.compile("^[a-zA-Z]+(([',. -][a-zA-Z ])?[a-zA-Z])$").matcher(s).matches();
+    }
+
+    //regex for phone
+    private boolean phoneIsValid(String s) {
+        return Pattern.compile("^01[0125][0-9]{8}").matcher(s).matches();
+    }
+
+    //regex for email
+    private boolean emailIsValid(String s) {
+        return Pattern.compile("^(.+)@(.+)$").matcher(s).matches();
+    }
 
     private void requestStoragePermission() {
         requestPermissions(new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE, Manifest.permission.WRITE_EXTERNAL_STORAGE}, 100);
     }
-
     private void requestCameraPermission() {
         requestPermissions(new String[]{Manifest.permission.CAMERA, Manifest.permission.WRITE_EXTERNAL_STORAGE}, 100);
     }
-
     private boolean checkCameraPermission() {
         boolean res1 = ContextCompat.checkSelfPermission(getContext(), Manifest.permission.CAMERA) == PackageManager.PERMISSION_GRANTED;
         boolean res2 = ContextCompat.checkSelfPermission(getContext(), Manifest.permission.WRITE_EXTERNAL_STORAGE) == PackageManager.PERMISSION_GRANTED;
         return res1 && res2;
     }
-
     private boolean checkStoragePermission() {
         boolean res2 = ContextCompat.checkSelfPermission(getContext(), Manifest.permission.WRITE_EXTERNAL_STORAGE) == PackageManager.PERMISSION_GRANTED;
         return res2;
     }
-
     private void pickImage() {
         CropImage.activity()
                 .start(getContext(), this);
     }
-
     @Override
     public void onActivityResult(int requestCode, int resultCode, Intent data) {
         if (requestCode == CropImage.CROP_IMAGE_ACTIVITY_REQUEST_CODE) {
@@ -185,6 +235,7 @@ public class ProfileFragment extends Fragment {
             if (resultCode == Activity.RESULT_OK) {
                 imageUri = result.getUri();
                 Picasso.get().load(imageUri).into(profileImage);
+                update.setEnabled(true);
                 addToStorage(imageUri);
 
             } else if (resultCode == CropImage.CROP_IMAGE_ACTIVITY_RESULT_ERROR_CODE) {
@@ -192,7 +243,6 @@ public class ProfileFragment extends Fragment {
             }
         }
     }
-
     private void addToStorage(Uri uri) {
         storageReference = FirebaseStorage.getInstance().getReference().child("MyImages");
         final StorageReference imageName = storageReference.child("Image1234");
@@ -205,11 +255,19 @@ public class ProfileFragment extends Fragment {
                         reference = FirebaseDatabase.getInstance()
                                 .getReference("Users").child(auth.getCurrentUser().getUid()).child("uri");
                         reference.setValue(uri.toString());
-
                     }
                 });
             }
         });
     }
-
+    private boolean isConnected(){
+        ConnectivityManager connectivityManager=(ConnectivityManager)getActivity().getSystemService(Context.CONNECTIVITY_SERVICE);
+        NetworkInfo wifiConn =connectivityManager.getNetworkInfo(ConnectivityManager.TYPE_WIFI);
+        NetworkInfo mobileConn =connectivityManager.getNetworkInfo(ConnectivityManager.TYPE_MOBILE);
+        if ((wifiConn!=null && wifiConn.isConnected()) ||(mobileConn!=null && mobileConn.isConnected())){
+            return true;
+        }else {
+            return false;
+        }
+    }
 }
